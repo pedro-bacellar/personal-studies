fea1d132918a87a6bf7357b082df6a4ad77ea37a
Pedro Bacellar <pedrofrancabacellar@gmail.com>
2025-11-28 15:25:53 -0300

    ✨ feat: add lesson 24 - break and continue

---
+++ java-bro-code/BreakAndContinue/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    static void main(String[] args) {
+
+        for(int i = 0; i < 10; i++){
+            if (i == 4){
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < 10; i++){
+            if (i == 5){
+                break;
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
