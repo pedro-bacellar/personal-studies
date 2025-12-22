aa810aa700b6cde4d4d0e7c9bdd8fd0fa4d830b6
Pedro Bacellar <pedrofrancabacellar@gmail.com>
2025-12-22 18:12:28 -0300

    ♻️ refactor: simplify fizzbuzz logic

--- java-exercises/FizzBuzz/src/Main.java
+++ java-exercises/FizzBuzz/src/Main.java
@@ -1,25 +1,22 @@
 public class Main {
     public static void main(String[] args){
 
-        int numbers = 0;
+        for(int i = 1; i <= 100; i++){
 
-        for(int i = 0; i < 100; i++){
-            numbers = i + 1;
-
-            if(numbers % 3 == 0 && numbers % 5 == 0){
+            if(i % 3 == 0 && i % 5 == 0){
                 System.out.printf("FizzBuzz\n");
                 continue;
             }
-            else if(numbers % 5 == 0){
+            else if(i % 5 == 0){
                 System.out.printf("Buzz\n");
                 continue;
             }
-            else if(numbers % 3 == 0) {
+            else if(i % 3 == 0) {
                 System.out.printf("Fizz\n");
                 continue;
             }
             else{
-                System.out.println(numbers);
+                System.out.println(i);
             }
         }
     }
