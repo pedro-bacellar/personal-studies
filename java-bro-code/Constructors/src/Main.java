79f149c55227dd42afdebdfc50d47acb04eb9a56
Pedro Bacellar <pedrofrancabacellar@gmail.com>
2026-01-11 14:14:12 -0300

    ✨ feat: add constructors lesson

---
+++ java-bro-code/Constructors/src/Main.java
@@ -0,0 +1,34 @@
+            // 40. constructors
+
+public class Main {
+    public static void main(String[] args){
+
+        Student student1 = new Student("Spongebob", 30, 3.2);
+        Student student2 = new Student("Patrick", 34, 1.5);
+        Student student3 = new Student("Sandy", 27, 4.0);
+
+        student1.study();
+        System.out.println(student1.name);
+        System.out.println(student1.age);
+        System.out.println(student1.gpa);
+        System.out.println(student1.isEnrolled);
+        System.out.println();
+
+        student2.study();
+        System.out.println(student2.name);
+        System.out.println(student2.age);
+        System.out.println(student2.gpa);
+        System.out.println(student2.isEnrolled);
+        System.out.println();
+
+        student3.study();
+        System.out.println(student3.name);
+        System.out.println(student3.age);
+        System.out.println(student3.gpa);
+        System.out.println(student3.isEnrolled);
+
+
+
+
+    }
+}
---
+++ java-exercises/MiniQuizGame/src/Question.java
@@ -0,0 +1,6 @@
+public class Question {
+
+    String q1 = "What is Java?";
+    String[] options = {"A) An operating system", "B) A programming language", "C) A database", "D) A text editor"};
+    int answer = 1;
+}
