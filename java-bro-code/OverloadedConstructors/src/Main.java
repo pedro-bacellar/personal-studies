634139ce091679cb5a6129f70ef1c35f63c0219d
Pedro Bacellar <pedrofrancabacellar@gmail.com>
2026-01-12 12:02:20 -0300

    ✨ feat: add overloaded constructors lesson

---
+++ java-bro-code/OverloadedConstructors/src/Main.java
@@ -0,0 +1,28 @@
+            // 41. overloaded constructors
+
+public class Main {
+    public static void main(String[] args){
+
+        User user1 = new User("Spongebob");
+        User user2 = new User("Patrick", "PStar@aol.com");
+        User user3 = new User("Sandy", "SCheeks@gmail.com", 27);
+        User user4 = new User();
+
+        System.out.println(user1.username);
+        System.out.println(user1.email);
+        System.out.println(user1.age);
+
+        System.out.println(user2.username);
+        System.out.println(user2.email);
+        System.out.println(user2.age);
+
+        System.out.println(user3.username);
+        System.out.println(user3.email);
+        System.out.println(user3.age);
+
+        System.out.println(user4.username);
+        System.out.println(user4.email);
+        System.out.println(user4.age);
+
+    }
+}
