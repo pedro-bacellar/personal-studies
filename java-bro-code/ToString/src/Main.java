f2634faace5443439dab6d89f9fe7021e0265d3a
Pedro Bacellar <pedrofrancabacellar@gmail.com>
2026-01-13 18:52:06 -0300

    ✨ feat: add toString lesson

---
+++ java-bro-code/ToString/src/Main.java
@@ -0,0 +1,12 @@
+            // 47. tostring
+
+public class Main {
+    public static void main(String[] args){
+
+        Car car1 = new Car("Ford", "Mustang", 2025, "Red");
+        Car car2 = new Car("Chevrolet", "Corvette", 2026, "Blue");
+
+        System.out.println(car1);
+        System.out.println(car2);
+    }
+}
