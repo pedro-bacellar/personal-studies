7aa1d5d003e991fa53dfb969ba0aaa198e8a7c50
Pedro Bacellar <pedrofrancabacellar@gmail.com>
2026-01-13 18:39:12 -0300

    ✨ feat: add method overriding lesson

---
+++ java-bro-code/MethodOverriding/src/Main.java
@@ -0,0 +1,14 @@
+            // 46. method overriding
+
+public class Main {
+    public static void main(String[] args){
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Fish fish = new Fish();
+
+        dog.move();
+        cat.move();
+        fish.move();
+    }
+}
