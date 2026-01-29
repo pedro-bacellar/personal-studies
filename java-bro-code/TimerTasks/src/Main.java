44ffa9cc334fa71f69364a9665d892906d41f9b4
Pedro Bacellar <pedrofrancabacellar@gmail.com>
2026-01-29 10:23:53 -0300

    ✨ feat: add timertasks lesson

---
+++ java-bro-code/TimerTasks/src/Main.java
@@ -0,0 +1,26 @@
+            // 64. timertasks
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask(){
+
+            int count = 3;
+
+            @Override
+            public void run(){
+                System.out.println("Hello");
+                count--;
+                if(count <= 0){
+                    System.out.println("TASK COMPLETE!");
+                    timer.cancel();
+                }
+            }
+        };
+        timer.schedule(task, 0, 1000);
+    }
+}
