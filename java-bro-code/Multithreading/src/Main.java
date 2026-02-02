ef0bfe599bbfeebe42eea313cf2bdc116965fd38
Pedro Bacellar <pedrofrancabacellar@gmail.com>
2026-02-02 17:25:22 -0300

    ✨ feat: add multithreading lesson

---
+++ java-bro-code/Multithreading/src/Main.java
@@ -0,0 +1,24 @@
+            // 70. multithreading
+
+public class Main {
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread(new MyRunnable("PING"));
+        Thread thread2 = new Thread(new MyRunnable("PONG"));
+
+        System.out.println("GAME START!");
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        }
+        catch (InterruptedException e) {
+            System.out.println("Main thread was interrupted");
+        }
+
+        System.out.println("GAME OVER!");
+    }
+}
